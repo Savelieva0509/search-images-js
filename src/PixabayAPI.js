d5cc2b803b89514483196b072b9f6d686036112f
Babenko Liudmyla <savelieva0509@gmail.com>
2022-10-14 18:34:18 +0300

    task 11

--- src/PixabayAPI.js
+++ src/PixabayAPI.js
@@ -1,11 +1,13 @@
 export class PixabayAPI {
 
     #page = 1;
-    #searchQuery = '';
+    #query = '';
+    #totalPages = 0;
+    #perPage = 40;
 
     getPhotos() {
         const url =
-            `https://pixabay.com/api/?key=30573092-ee1d5b60b5a26f0dd8b37d65c&q=${this.#searchQuery}&image_type=photo&per_page=40&orientation=horizontal&safesearch=true&page=${this.#page}`
+            `https://pixabay.com/api/?key=30573092-ee1d5b60b5a26f0dd8b37d65c&q=${this.#query}&image_type=photo&per_page=${this.#perPage}&orientation=horizontal&safesearch=true&page=${this.#page}`
         return fetch(url).then(response => {
             if (!response.ok) {
                 throw new Error(response.status);
@@ -14,15 +16,27 @@ export class PixabayAPI {
         })
     }
 
-    set search (newSearch) {
-        this.#searchQuery = newSearch;
+    set query (newQuery) {
+        this.#query = newQuery;
     }
 
-     get search () {
-         return this.#searchQuery;
+     get query () {
+         return this.#query;
     }
 
     incrementPage() {
         this.#page +=1
+    }
+
+    resetPage() {
+        this.#page = 1;
+    }
+
+    calculateTotalPages(total) {
+        this.#totalPages = Math.ceil(total / this.#perPage);
+    }
+
+    get isShowLoadMore() {
+        return this.#page < this.#totalPages;
     }
 }
--- src/index.js
+++ src/index.js
@@ -9,34 +9,66 @@ const handleSubmit = (event) => {
   event.preventDefault()
     
     const {
-        elements: {searchQuery},
-    } = event.currentTarget
+        elements: { searchQuery },
+    } = event.currentTarget;
     const search = searchQuery.value.trim().toLowerCase();
-    console.log(search);
+
     if (!search) {
-        Notify.failure (`Enter data to search!`)
+        Notify.failure(`Enter data to search!`);
         return;
-        
     }
     
-    pixabay.searchQuery = search;
-    pixabay.getPhotos(search).then(({hits}) => {
-        const markup = createMarkup(hits); 
-        console.log(markup);
-        refs.list.insertAdjacentHTML('beforeend',markup)
-    });
-   
+    pixabay.query = search;
 
+    clearPage();
+
+    pixabay
+        .getPhotos(search)
+        .then(({ hits, total }) => {
+
+        if (hits.length === 0) {
+            Notify.info(`No images found for your '${search}' request.`)
+            return;
+        }
+            
+        const markup = createMarkup(hits);
+        refs.list.insertAdjacentHTML('beforeend', markup);
+        
+        pixabay.calculateTotalPages(total);
+
+        Notify.success(`We found ${total} images by request '${search}'.`)
+        if (pixabay.isShowLoadMore) {
+            refs.loadMoreBtn.classList.remove('is-hidden');
+        }
+    }).catch(error => {
+        Notify.error('Something goes wrong');
+        clearPage();
+    });
 }
+
 const onLoadMore = () => {
     pixabay.incrementPage();
+    
+    if (!pixabay.isShowLoadMore) {
+        refs.loadMoreBtn.classList.add('is-hidden');
+        }
 
-    pixabay.getPhotos().then(({ hits }) => {
+    pixabay.getPhotos().then(({ hits, }) => {
         const markup = createMarkup(hits);
-        console.log(markup);
-        refs.list.insertAdjacentHTML('beforeend', markup)
+   
+        refs.list.insertAdjacentHTML('beforeend', markup);
+        
+    }).catch(error => {
+        Notify.error('Something goes wrong');
+        clearPage();
     });
 };
 
 refs.form.addEventListener("submit", handleSubmit);
-refs.loadMoreBtn.addEventListener('click', onLoadMore)
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+function clearPage() {
+    pixabay.resetPage()
+    refs.list.innerHTML = '';
+    refs.loadMoreBtn.classList.add('is-hidden');
+}
