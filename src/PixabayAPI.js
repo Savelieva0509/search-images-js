202f9173d3470f99f7aee4453ef46351ad876ce4
Babenko Liudmyla <savelieva0509@gmail.com>
2022-10-20 18:19:56 +0300

    исправила гит конфликт

--- src/PixabayAPI.js
+++ src/PixabayAPI.js
@@ -1,13 +1,6 @@
 import axios from 'axios';
 
-<<<<<<< HEAD
 const BASE_URL = 'https://pixabay.com/api/';
-=======
-axios.defaults.baseURL = 'https://pixabay.com/api/';
-// axios.defaults.headers.common['Authorization'] = '30573092-ee1d5b60b5a26f0dd8b37d65c';
-
-// const BASE_URL = 'https://pixabay.com/api/';
->>>>>>> 05b4b256943acb4d7325acd546995d4efbda0936
 const API_KEY = '30573092-ee1d5b60b5a26f0dd8b37d65c';
 
 export class PixabayAPI {
@@ -18,7 +11,6 @@ export class PixabayAPI {
     #perPage = 40;
     #params = {
         params: {
-<<<<<<< HEAD
             key: API_KEY,
             image_type: 'photo',
             orientation: 'horizontal',
@@ -31,25 +23,8 @@ export class PixabayAPI {
             `${BASE_URL}?${API_KEY}&q=${this.#query}&per_page=${this.#perPage}&page=${this.#page}`
         const {data} = await axios.get(url,this.#params);
         return data;
-=======
-            key: API_KEY,   
-            // q: this.query,
-            image_type: 'photo',
-            orientation: 'horizontal',
-            safesearch: true,
-            page: this.page,
-            per_page: this.perPage,
-  },
-    }
-
-    async getPhotos () {
-         const url =
-            `?q=${this.#query}`
-        const {data} = await axios.get(url,this.#params);
-        return data;      
->>>>>>> 05b4b256943acb4d7325acd546995d4efbda0936
-    }
-
+  }
+   
     set query (newQuery) {
         this.#query = newQuery;
     }
--- src/createMarkup.js
+++ src/createMarkup.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-=======
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
->>>>>>> 05b4b256943acb4d7325acd546995d4efbda0936
 
 export function createMarkup(photos) {
     return photos.map(({webformatURL ,largeImageURL, tags,likes,views,comments,downloads}) => {
@@ -30,22 +25,3 @@ export function createMarkup(photos) {
     }).join("")
 }
  
-
-export function simpleLightbox() {
-  let lightbox = new SimpleLightbox('.gallery', {
-    captionsData: "alt",
-    captionDelay: 250,
-  });
-  lightbox.refresh();
-}
-
-export function scroll() {
-  const { height: cardHeight } = document
-    .querySelector('.gallery')
-    .firstElementChild.getBoundingClientRect();
-
-  window.scrollBy({
-    top: cardHeight * 2,
-    behavior: 'smooth',
-  });
-}
