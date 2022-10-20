33534e8d2762ae4f3c8b40302c4293810a0302fa
Babenko Liudmyla <savelieva0509@gmail.com>
2022-10-20 19:34:13 +0300

    библиотека + плавный скролл

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import { simpleLightbox, scroll } from './simple_scroll';
 
 const pixabay = new PixabayAPI();
 
-const handleSubmit = (event) => {
-  event.preventDefault()
+const handleSubmit = async (event) => {
+    event.preventDefault()
     
     const {
         elements: { searchQuery },
@@ -19,22 +19,22 @@ const handleSubmit = (event) => {
         Notify.failure(`Enter data to search!`);
         return;
     }
-    
+
     pixabay.query = search;
 
     clearPage();
 
-    pixabay
-        .getPhotos(search)
-        .then(({ hits, total }) => {
-
+    try {
+        const { hits, total } = await pixabay.getPhotos();
         if (hits.length === 0) {
             Notify.info(`No images found for your '${search}' request.`)
             return;
         }
-            
         const markup = createMarkup(hits);
         refs.list.insertAdjacentHTML('beforeend', markup);
+        
+        simpleLightbox();
+        scroll();
         
         pixabay.calculateTotalPages(total);
 
@@ -42,35 +42,58 @@ const handleSubmit = (event) => {
         if (pixabay.isShowLoadMore) {
             refs.loadMoreBtn.classList.remove('is-hidden');
         }
-    }).catch(error => {
+    } catch (error) {
         Notify.error('Something goes wrong');
         clearPage();
-    });
-}
+    }
+};
+    //     pixabay
+    //         .getPhotos(search)
+    //         .then(({ hits, total }) => {
 
-const onLoadMore = () => {
-    pixabay.incrementPage();
+    //         if (hits.length === 0) {
+    //             Notify.info(`No images found for your '${search}' request.`)
+    //             return;
+    //         }
+            
+    //         const markup = createMarkup(hits);
+    //         refs.list.insertAdjacentHTML('beforeend', markup);
+        
+    //         pixabay.calculateTotalPages(total);
+
+    //         Notify.success(`We found ${total} images by request '${search}'.`)
+    //         if (pixabay.isShowLoadMore) {
+    //             refs.loadMoreBtn.classList.remove('is-hidden');
+    //         }
+    //     }).catch(error => {
+    //         Notify.error('Something goes wrong');
+    //         clearPage();
+    //     });
+    // }
+
+    const onLoadMore = () => {
+        pixabay.incrementPage();
     
-    if (!pixabay.isShowLoadMore) {
-        refs.loadMoreBtn.classList.add('is-hidden');
+        if (!pixabay.isShowLoadMore) {
+            refs.loadMoreBtn.classList.add('is-hidden');
         }
 
-    pixabay.getPhotos().then(({ hits, }) => {
-        const markup = createMarkup(hits);
+        pixabay.getPhotos().then(({ hits, }) => {
+            const markup = createMarkup(hits);
    
-        refs.list.insertAdjacentHTML('beforeend', markup);
+            refs.list.insertAdjacentHTML('beforeend', markup);
         
-    }).catch(error => {
-        Notify.error('Something goes wrong');
-        clearPage();
-    });
-};
+        }).catch(error => {
+            Notify.error('Something goes wrong');
+            clearPage();
+        });
+    };
 
-refs.form.addEventListener("submit", handleSubmit);
-refs.loadMoreBtn.addEventListener('click', onLoadMore);
+    refs.form.addEventListener("submit", handleSubmit);
+    refs.loadMoreBtn.addEventListener('click', onLoadMore);
 
-function clearPage() {
-    pixabay.resetPage()
-    refs.list.innerHTML = '';
-    refs.loadMoreBtn.classList.add('is-hidden');
-}
+    function clearPage() {
+        pixabay.resetPage()
+        refs.list.innerHTML = '';
+        refs.loadMoreBtn.classList.add('is-hidden');
+    }
--- src/simple_scroll.js
+++ src/simple_scroll.js
@@ -2,7 +2,7 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 export function simpleLightbox() {
-  let lightbox = new SimpleLightbox('.gallery', {
+  let lightbox = new SimpleLightbox('.gallery a', {
     captionsData: "alt",
     captionDelay: 250,
   });
@@ -10,12 +10,12 @@ export function simpleLightbox() {
 }
 
 export function scroll() {
-  const { height: cardHeight } = document
-    .querySelector('.gallery')
-    .firstElementChild.getBoundingClientRect();
+const { height: cardHeight } = document
+  .querySelector(".gallery")
+  .firstElementChild.getBoundingClientRect();
 
-  window.scrollBy({
-    top: cardHeight * 2,
-    behavior: 'smooth',
-  });
+window.scrollBy({
+  top: cardHeight * 2,
+  behavior: "smooth",
+});
 }
